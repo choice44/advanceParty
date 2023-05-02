7b38f2254625ea1c9ac39d3394a85a0e31b4d839
Choi Seungwon <cso6549@naver.com>
2023-05-02 16:54:11 +0900

    improve pascals_triangle

--- 3rd_week/pascals_triangle.py
+++ 3rd_week/pascals_triangle.py
@@ -2,13 +2,14 @@ def pascals_triangle(n):
     if n == 1:
         return [1]
     nth_list = []
+    previous_pt = pascals_triangle(n - 1)
     for i in range(n):
         if i == 0:
-            nth_list.append(pascals_triangle(n - 1)[0])
+            nth_list.append(previous_pt[0])
         elif i == n - 1:
-            nth_list.append(pascals_triangle(n - 1)[-1])
+            nth_list.append(previous_pt[-1])
         else:
-            nth_list.append(pascals_triangle(n - 1)[i - 1] + pascals_triangle(n - 1)[i])
+            nth_list.append(previous_pt[i - 1] + previous_pt[i])
     return nth_list
 
 
